a7b3c6158a190c28540e5caa81fc6575faf6f855
Milo <79590178+e1r2t374@users.noreply.github.com>
2024-04-09 10:25:06 -0500

    Update and rename Java/ArrayLists.java to Java/ArrayLists/Main.java

--- Java/ArrayLists/Main.java
+++ Java/ArrayLists/Main.java
@@ -6,7 +6,7 @@
 */
 
 import java.util.*;
-public class ArrayLists {
+public class Main {
   public static void main(String[] args) {
     ArrayList<Integer> list = new ArrayList<Integer>();
     list.add(4);list.add(-1);list.add(-2);list.add(4);list.add(5);list.add(-2);
