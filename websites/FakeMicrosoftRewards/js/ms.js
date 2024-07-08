7492d98ccb1fa06640563e1329370b5757c34311
Milo <79590178+e1r2t374@users.noreply.github.com>
2024-07-08 10:11:43 -0500

    Delete websites directory

--- websites/FakeMicrosoftRewards/js/ms.js
+++
@@ -1,7 +0,0 @@
-function exe1() {
-  window.location.replace("https://duftmf.csb.app/sign.html");
-}
-
-document.querySelector("#helpicon").onclick = function () {
-  window.open("https://support.google.com/opinionrewards?hl=en", "_blank");
-};
